5a35356627f23821804f0d51455226d7089bfbd0
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-03-12 22:49:34 -0300

    Clase conexión.

--- src/java/controlador/Conexion.java
+++ src/java/controlador/Conexion.java
@@ -5,10 +5,39 @@
  */
 package controlador;
 
-/**
- *
+
+import java.sql.*;
+ 
+/*
  * @author Prophet
  */
+
 public class Conexion {
+     
+    Connection con;
     
+    public void abrirConexion(){
+
+    try 
+    {   
+      String url = "jdbc:mysql://localhost:3306/WS?zeroDateTimeBehavior=convertToNull";
+      String us = "root";
+      String psw = "";
+      con = DriverManager.getConnection(url, us, psw);
+    } 
+    
+    catch (SQLException e) {
+            System.out.println("Error en la conexion"+e);
+        }
+    }
+    
+    public void cerrarConexion(){
+        try 
+        {
+           con.close();
+        } 
+        catch (SQLException e) {
+            System.out.println("Error al cerrar conexion" +e);
+        }
+    }
 }
